4b4674418e5c49fe021c1146fb50524f78eafa7b
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-11 17:56:46 +0300

    server-client simple first connection tested and succeed

---
+++ Chatt'n/Assets/Scripts/Test/ClientTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ClientSide;
+public class ClientTester : MonoBehaviour
+{
+    Client client;
+    private void Awake()
+    {
+        client = new Client();
+    }
+    public void Connect()
+    {
+        client.Connect();
+    }
+
+}
