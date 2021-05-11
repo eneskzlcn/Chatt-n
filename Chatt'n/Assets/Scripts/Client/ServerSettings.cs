a093bfe8d02ae746ac23cba9305ad9b407a0e18b
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-11 17:45:47 +0300

    constant Server Settings created

---
+++ Chatt'n/Assets/Scripts/Client/ServerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClientSide.ServerSettings
+{
+    public static class ServerSettings
+    {
+        public static string HOST = "127.0.0.1";
+
+        public static int PORT = 5000;
+    }
+}
