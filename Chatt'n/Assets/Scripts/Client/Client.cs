f1f778fa34690e49b025d4fa0cf3d9c94641cc57
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-11 17:45:30 +0300

    A simple client that just connects a server is created

---
+++ Chatt'n/Assets/Scripts/Client/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSide.Client
+{
+    public class Client
+    {
+        private TcpClient socket;
+
+        public Client()
+        {
+            socket = new TcpClient();
+        }
+        public void Connect()
+        {
+            socket.BeginConnect(ServerSettings.HOST,ServerSettings.PORT,new AsyncCallback(onBeginConnect),null);
+        }
+
+        public void onBeginConnect(IAsyncResult ar)
+        {
+            socket.EndConnect(ar);
+            Debug.Log("Connected to the server");
+        }
+    }
+}
