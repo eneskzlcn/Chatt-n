77d2a2e17003c209e1b978655009ebcb9a2bb5cd
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-12 13:47:12 +0300

    SClient has id , username and socket now

--- Chatt'n/Assets/Scripts/Server/SClient.cs
+++ Chatt'n/Assets/Scripts/Server/SClient.cs
@@ -1,5 +1,21 @@
-﻿
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+// The purpose of thiss class is represent any client as a class in server-side
 public class SClient 
 {
+    public TcpClient socket;
+
+    //this id will allow us to reach the client from the dic in server directly ( mapped with ids).
+    public int _clientID;
     
+    public string _userName;
+
+    public SClient(int clientID,TcpClient socket )
+    {
+        this.socket = socket;
+        this._clientID = clientID;
+    }
 }
