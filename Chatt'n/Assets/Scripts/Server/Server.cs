36e8df61e34a79ea02c8b8ce2100d4d875f0302f
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-12 13:06:54 +0300

    server now full of static context. Reachable from anywhere

--- Chatt'n/Assets/Scripts/Server/Server.cs
+++ Chatt'n/Assets/Scripts/Server/Server.cs
@@ -4,41 +4,30 @@ using UnityEngine;
 using System;
 using System.Net;
 using System.Net.Sockets;
-public class Server : MonoBehaviour
+
+namespace ServerSide
 {
-    public string _ip;
-    public int _port;
-    public int _maximumClientCount;
-    //TcpListener starts a listener that listends for incoming clients.
-    public TcpListener serverListener;
-
-    private void Awake()
+    public static class Server
     {
-        Initialize();
-    }
-    public void Initialize()
-    {
-        serverListener = new TcpListener(IPAddress.Any,_port); //IPAddress.Any provides accepting every IP.
+        //TcpListener starts a listener that listends for incoming clients.
+        private static TcpListener serverListener = new TcpListener(IPAddress.Any,ServerSettings.PORT);
 
-        Debug.Log($"Server created. Listening on :{_port} port. Maximum client count : {_maximumClientCount}"); 
-    }
+        
+        public static void Start()
+        {
+            serverListener.Start();
+            Debug.Log("Server started"); 
+            serverListener.BeginAcceptTcpClient(onBeginAcceptTcpClient,null);
+        }
 
-    private void Start()
-    {
-        serverListener.Start();
-        Debug.Log("Server started"); 
-
-        serverListener.BeginAcceptTcpClient(onBeginAcceptTcpClient,null);
-    }
-
-    public void onBeginAcceptTcpClient(IAsyncResult ar)
-    {
-        Debug.Log("Started to accept new socket...");
-        TcpClient newClient = serverListener.EndAcceptTcpClient(ar);
-        Debug.Log($"Accepted Client : {newClient.Client.RemoteEndPoint}");
-
-        // after a client accepted or rejected we must continue for waiting for new clients. So,
-        // if we use begin accept method in here then there will be a loop never stops waiting for new clients
-        serverListener.BeginAcceptTcpClient(onBeginAcceptTcpClient,null);
+        private static void onBeginAcceptTcpClient(IAsyncResult ar)
+        {
+            Debug.Log("Started to accept new socket...");
+            TcpClient newClient = serverListener.EndAcceptTcpClient(ar);
+            Debug.Log($"Accepted Client : {newClient.Client.RemoteEndPoint}");
+            // after a client accepted or rejected we must continue for waiting for new clients. So,
+            // if we use begin accept method in here then there will be a loop never stops waiting for new clients
+            serverListener.BeginAcceptTcpClient(onBeginAcceptTcpClient,null);
+        }
     }
 }
