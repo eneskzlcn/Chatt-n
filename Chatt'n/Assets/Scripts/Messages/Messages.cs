8f9deb5f7eec8755d454347fe81fd88b4800e9ff
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-13 07:30:49 +0300

    Messages moved in different folder and namespace

---
+++ Chatt'n/Assets/Scripts/Messages/Messages.cs
@@ -0,0 +1,15 @@
+
+namespace Messages
+{
+    public enum Message_Type
+    {
+        CONNECTED,DISCONNECTED
+    }
+    public struct Message
+    {
+        public Message_Type type;
+        public string content; // maybe another struct (json form) .
+    }
+
+    
+}
