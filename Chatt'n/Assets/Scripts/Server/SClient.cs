503d38f18521f4d685c5ddc11bc8c37c0cd14854
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-13 07:30:24 +0300

    Send Message  (writing message ) added to SClient

--- Chatt'n/Assets/Scripts/Server/SClient.cs
+++ Chatt'n/Assets/Scripts/Server/SClient.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-
+using System.Text;
+using UnityEngine;
 // The purpose of thiss class is represent any client as a class in server-side
 namespace ServerSide
 {
@@ -31,12 +32,12 @@ namespace ServerSide
 
             
         }       
-        public ReadIncomingData()
+        public void ReadIncomingData()
         {
             _stream.BeginRead(_buffer,0,_buffer.Length,new AsyncCallback(OnReadIncomingData),null);
         }
 
-        public OnReadIncomingData(IAsyncResult ar)
+        public void OnReadIncomingData(IAsyncResult ar)
         {
             try
             {
@@ -49,22 +50,34 @@ namespace ServerSide
                 {
                     //There is an error occured
                     //Disconnect or break
+                    Debug.Log("There is an error occured on reading.");
+
                     return;
                 }
                 //we initalize a temp data array because of the _buffer not completely full or clear.
                 // the _buffer always accepts data amount of our BUFFER_SIZE but the incoming data not
                 //always equal to BUFFER_SIZE. So we starts an array that exactly the same length with given
                 //data = _data.
-                byte _data = new byte[_incomingDataLength];
+                byte[] _data = new byte[_incomingDataLength];
                 //Array.Copy provides that the empty or broken bytes not being copied to the new array _data
-                Array.Copy(buffer,_data,_incomingDataLength);
-                //
+                Array.Copy(_buffer,_data,_incomingDataLength);
+                string _incomingString = Encoding.UTF8.GetString(_buffer);
+                Debug.Log("I am SClient. My id:"+this._clientID+". Coming string : "+_incomingString);
             }
             catch (System.Exception)
             {
                 
-                throw;
+                Debug.Log("System.Exception SClient :70");
             }
+        }
+        public void SendMessage(string message)
+        {
+            byte[] sendingData = Encoding.UTF8.GetBytes(message);
+            _stream.BeginWrite(sendingData,0,sendingData.Length,OnSendingData,null);
+        }
+        public void OnSendingData(IAsyncResult ar)
+        {
+             _stream.EndWrite(ar);
         }
     }
 }
