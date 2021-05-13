fc38a3751cf428ae7395d667c980b90dc9728135
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-13 08:14:57 +0300

    converting the messge object to json and sending to server test created

--- Chatt'n/Assets/Scripts/Test/ClientTester.cs
+++ Chatt'n/Assets/Scripts/Test/ClientTester.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ClientSide;
-
+using Newtonsoft.Json;
+using Messages;
 namespace Test
 {
 
@@ -17,7 +18,11 @@ namespace Test
 
         public void Send()
         {
-            client.SendMessage("asd");
+            Message message = new Message();
+            message.type = Message_Type.USERNAME;
+            message.content = "Bizarre Cabal";
+            string json_message = JsonConvert.SerializeObject(message);
+            client.SendMessage(json_message);
         }
     }
 }
