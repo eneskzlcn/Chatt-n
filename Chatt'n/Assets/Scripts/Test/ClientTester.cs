cd6816c61c0e028be2bf19261cdf19d785262f26
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-14 21:42:22 +0300

    func. changed to create a new client on each click

--- Chatt'n/Assets/Scripts/Test/ClientTester.cs
+++ Chatt'n/Assets/Scripts/Test/ClientTester.cs
@@ -9,20 +9,10 @@ namespace Test
 
     public class ClientTester : MonoBehaviour
     {
-        Client client;
-        public void Connect()
-        {
-            client = new Client();
-            client.Connect();
-        }
-
-        public void Send()
-        {
-            Message message = new Message();
-            message.type = Message_Type.USERNAME;
-            message.content = "Bizarre Cabal";
-            string json_message = JsonConvert.SerializeObject(message);
-            client.SendMessage(json_message);
-        }
+       public GameObject clientPrefab;
+       public void CreateNewClient()
+       {
+            Instantiate(clientPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+       }
     }
 }
