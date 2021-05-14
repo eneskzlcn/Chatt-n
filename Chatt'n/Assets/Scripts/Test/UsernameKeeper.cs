8769a5ddc75f597ad22697ee9614318cf49d5d08
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-14 21:42:07 +0300

    Username keeper scriptable object created

---
+++ Chatt'n/Assets/Scripts/Test/UsernameKeeper.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName= "Username Keeper",menuName="ScriptableObjects/Username Keeper")]
+public class UsernameKeeper : ScriptableObject {
+    public string userName;
+}
