d7c83dc28d4fd4d19ae431a0077e771c6cc3a516
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-17 14:45:05 +0300

    connection to the server providing by 'Connect' button now

--- Chat'n/Assets/Scripts/Test/LoginMenuController.cs
+++ Chat'n/Assets/Scripts/Test/LoginMenuController.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
+using ClientSide;
+using Messages;
+using Newtonsoft.Json;
 
 public class LoginMenuController : MonoBehaviour {
     public Button _loginButton;
@@ -14,5 +17,15 @@ public class LoginMenuController : MonoBehaviour {
         {
             _loginButton.interactable = false;
         }
+    }
+
+    public void Connect()
+    {
+        Client.Connect();
+        Message message = new Message();
+        message.type = Message_Type.USERNAME;
+        message.content = _userName.text.ToString();
+        string json_message = JsonConvert.SerializeObject(message);
+        Client.SendMessage(json_message);
     }
 }
