656d50abfe881affa8d09542e1d7e07d8f42bebb
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-05-18 14:25:48 +0300

    not need username keeper anymore. so deleted.

--- Chat'n/Assets/Scripts/Test/UsernameKeeper.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName= "Username Keeper",menuName="ScriptableObjects/Username Keeper")]
-public class UsernameKeeper : ScriptableObject {
-    public string userName;
-}
